8d0a3999869299a3f6f45183d768a30c41928f06
Jesse Paroz <jparoz@users.noreply.github.com>
2017-11-12 08:27:57 +1000

    entry! macro

--- src/bin/yakka.rs
+++ src/bin/yakka.rs
@@ -3,41 +3,38 @@ extern crate yakka;
 
 use yakka::Entry;
 
-fn entry(text: &str) -> Entry {
-    Entry {
-        text: text.to_string(),
-        children: Vec::new(),
-    }
+macro_rules! entry {
+    ($text:expr) => {
+        Entry {
+            text: $text.to_string(),
+            children: Vec::new(),
+        }
+    };
+    ($text:expr, [$($child:expr),+]) => {
+        {
+            let mut e = Entry {
+                text: $text.to_string(),
+                children: Vec::new(),
+            };
+            $(
+                e.children.push(entry!($child));
+            )+
+            e
+        }
+    };
 }
 
 fn main() {
     let mut root: Vec<Entry> = Vec::new();
-    root.push(entry("Lists"));
-    root.push(entry("Projects"));
-    root.push(entry("St Barts"));
-    root.push(entry("Scratch"));
-    root[0].children.push(entry("Bible"));
-    root[0].children.push(entry("Shopping"));
-    root[0].children.push(entry("Big spends"));
-    root[0].children.push(entry("Music"));
-    root[0].children.push(entry("Books"));
-    root[0].children.push(entry("Jokes"));
+
+    root.push(entry!("Lists",
+                     ["Bible", "Shopping", "Big spends", "Music", "Books", "Jokes"]));
+    root.push(entry!("Projects"));
+    root.push(entry!("St Barts"));
+    root.push(entry!("Scratch"));
     println!("root:");
     println!("{:?}", root);
 
-    println!();
-
-    // println!("serialized:");
     // let writer = std::io::stdout(); // @Temp
     // serde_json::to_writer(writer, &root).unwrap();
-
-    let serialized = serde_json::to_string(&root).unwrap();
-    println!("serialized:");
-    println!("{}", serialized);
-
-    println!();
-
-    let deserialized: Vec<Entry> = serde_json::from_str(&serialized).unwrap();
-    println!("deserialized:");
-    println!("{:?}", deserialized);
 }
