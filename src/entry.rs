278eda6d33a01f57b96be5afe2bed588f6b00360
Jesse Paroz <jparoz@users.noreply.github.com>
2017-11-15 22:30:59 +1000

    Scrap TUI. Not worth the jankiness for a bandaid

--- src/entry.rs
+++ src/entry.rs
@@ -4,8 +4,6 @@ use serde::de::{Visitor, SeqAccess, Error};
 
 use std::fmt;
 
-use super::SETTINGS;
-
 #[macro_use]
 macro_rules! entry {
     ($text:expr) => {
@@ -37,51 +35,18 @@ pub struct Entry {
     pub complete: bool,
 }
 
-impl Entry {
-    fn fmt_at_depth(&self, f: &mut fmt::Formatter, depth: usize) -> fmt::Result {
-        write!(f, "{}", "  ".repeat(depth))?;
-        write!(f, "{} ", SETTINGS.complete_char(self.complete))?;
-
-        let pad = (depth * 2) + 2;
-        let spaces = " ".repeat(pad);
-        let len = self.text.len();
-        let width = len.min(SETTINGS.width - pad);
-
-        // @StepBy
-        let mut i = 0;
-        loop {
-            let rem = len - i;
-            writeln!(f, "{}", &self.text[i..i + rem.min(width)])?;
-            i += width;
-            if i >= len {
-                break;
-            }
-            write!(f, "{}", spaces)?;
-        }
-
-        for child in self.children.iter() {
-            child.fmt_at_depth(f, depth + 1)?;
-        }
-        Ok(())
-    }
-}
-
-impl fmt::Display for Entry {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.fmt_at_depth(f, 0)
-    }
-}
-
 impl Serialize for Entry {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
         where S: Serializer
     {
         if self.children.is_empty() {
-            let mut tuple = serializer.serialize_tuple(1)?;
+            let mut tuple = serializer.serialize_tuple(2)?;
+            tuple.serialize_element(&self.complete)?;
             tuple.serialize_element(&self.text)?;
             tuple.end()
         } else {
-            let mut tuple = serializer.serialize_tuple(2)?;
+            let mut tuple = serializer.serialize_tuple(3)?;
+            tuple.serialize_element(&self.complete)?;
             tuple.serialize_element(&self.text)?;
             tuple.serialize_element(&self.children)?;
             tuple.end()
@@ -103,24 +68,28 @@ impl<'de> Visitor<'de> for EntryVisitor {
     type Value = Entry;
 
     fn expecting(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "a sequence with length 1 or 2")
+        write!(f, "a sequence with length 1, 2, or 3")
     }
 
     fn visit_seq<A>(self, mut seq: A) -> Result<Entry, A::Error>
         where A: SeqAccess<'de>
     {
+        let complete = match seq.next_element()? {
+            Some(complete) => complete,
+            None => return Err(Error::invalid_length(0, &self)),
+        };
         let text = match seq.next_element()? {
             Some(text) => text,
-            None => return Err(Error::invalid_length(0, &self)),
+            None => return Err(Error::invalid_length(1, &self)),
         };
         let children = match seq.next_element()? {
             Some(children) => children,
             None => Vec::new(),
         };
         Ok(Entry {
+            complete: complete,
             text: text,
             children: children,
-            complete: false,
         })
     }
 }
